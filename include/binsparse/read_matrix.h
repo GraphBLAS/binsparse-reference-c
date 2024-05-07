2c2b23a92e52936f84e5cd1917240db26f8856cf
Benjamin Brock <benjamin.brock@intel.com>
2024-05-07 14:33:48 -0700

    Ensure that format is known before parsing

--- include/binsparse/read_matrix.h
+++ include/binsparse/read_matrix.h
@@ -31,6 +31,8 @@ bsp_matrix_t bsp_read_matrix_from_group(hid_t f) {
 
   bsp_matrix_format_t format = bsp_get_matrix_format(format_string);
 
+  assert(format != 0);
+
   matrix.format = format;
 
   cJSON* nnz_ = cJSON_GetObjectItemCaseSensitive(binsparse, "nnz");
