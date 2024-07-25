bb40398492130801b293afda32fa0109b42e2b4b
Benjamin Brock <benjamin.brock@intel.com>
2024-07-25 13:18:15 -0700

    Update examples (#9)

--- examples/simple_matrix_read.c
+++ examples/simple_matrix_read.c
@@ -1,14 +1,14 @@
 #include <binsparse/binsparse.h>
 
 int main(int argc, char** argv) {
-  char* file_name = "test.hdf5";
+  const char* file_name = "test.hdf5";
 
   bsp_matrix_t mat = bsp_read_matrix(file_name, NULL);
 
   if (mat.format == BSP_COO) {
-    float* values = mat.values.data;
-    int* rowind = mat.indices_0.data;
-    int* colind = mat.indices_1.data;
+    float* values = (float*) mat.values.data;
+    int* rowind = (int*) mat.indices_0.data;
+    int* colind = (int*) mat.indices_1.data;
 
     for (size_t i = 0; i < mat.nnz; i++) {
       printf("%d, %d: %f\n", rowind[i], colind[i], values[i]);
